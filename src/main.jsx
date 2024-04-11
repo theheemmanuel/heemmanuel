2c682629d7e6314b6b48fca40762762f22245266
Emmanuel Oladiran <oladirane@gmail.com>
2024-04-11 19:06:15 +0100

    created rootlayout

--- src/main.jsx
+++ src/main.jsx
@@ -2,31 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Pages/Home.jsx";
-import About from "./Pages/About.jsx";
-import Projects from "./Pages/Projects.jsx";
-import Photos from "./Pages/Photos.jsx";
-import Contact from "./Pages/Contact.jsx";
-import Errorpage from "./Pages/Errorpage.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Errorpage />,
-    children: [
-      { path: "", element: <Home /> },
-      { path: "/about", element: <About /> },
-      { path: "/projects", element: <Projects /> },
-      { path: "/contact", element: <Contact /> },
-      { path: "/photos", element: <Photos /> },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
