4a1714ce3658ca3eef99d99cac301b0c8aacd18d
Emmanuel Oladiran <oladirane@gmail.com>
2024-04-19 22:00:17 +0100

    modified footer

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,20 +1,32 @@
-// import { FaReact } from "react-icons/fa";
 const Footer = () => {
+  // function Ola() {
+  //   return (
+  //     <div>
+  //       <h1>We are good to go</h1>
+  //     </div>
+  //   );
+  // }
+
   return (
     <div>
       <p className="bg-zinc-600 dark:bg-zinc-400 w-[100%] h-[0.5px]"></p>
       <div className="pt-6 pb-12 max-wid flex md:flex-row flex-col text-center justify-between items-center sticky bottom-0">
         <div className="font-semibold">
-          Built with: React.js ; Tailwind CSS ; Vercel
+          Built with: React.js; Tailwind CSS; Vercel
         </div>
         <h3>
           Design inspired by{" "}
-          <a className="underline text-blue-500" href="https://victoeke.com">
+          <a
+            target="_blank"
+            className="underline text-blue-500"
+            href="https://victoeke.com"
+          >
             Victor Eke
           </a>
+          <span>🙌</span>
         </h3>
         <div className="font-semibold">
-          Copyright © Emmanuel Oladiran 2024 All rights Reserved
+          Copyright &copy; Emmanuel Oladiran🧑‍💻 2024 All rights Reserved
         </div>
       </div>
     </div>
