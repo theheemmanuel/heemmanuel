62b83867ae006d2712db0c83f0ac04d22e3aecb4
theheemmanuel <oladirane@gmail.com>
2024-07-27 12:12:47 +0100

    links added

--- src/Pages/AllProject.js
+++ src/Pages/AllProject.js
@@ -1,7 +1,7 @@
-import pic1 from "../assets/blueLogo.png";
 import pic2 from "../assets/Annotation 2024-05-27 105818.png";
 import Trafalgar from "../assets/Trafalgar.jpg";
 import GetLinked from "../assets/GetLinked.jpg";
+import Get from "../assets/Get.png";
 import ENC from "../assets/ENCLINKS.png";
 import ENCdetails from "../assets/ENC.png";
 
@@ -12,18 +12,20 @@ const Allprojects = [
     subtitle: "Healthcare Landing page",
     image: pic2,
     main: Trafalgar,
+    link: "https://naridalo0311.netlify.app",
   },
   {
     id: 2,
     title: "GetLinked",
     subtitle: "A project for an Hackathon",
-    image: pic1,
+    image: Get,
     main: GetLinked,
+    link: "https://linkedget.netlify.app",
   },
   {
     id: 3,
     title: "ENCLINKS Tech",
-    subtitle: "Developed a landing page",
+    subtitle: "tech company landing page",
     image: ENC,
     main: ENCdetails,
     link: "https://enclinks.com",
--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,13 +1,12 @@
 const Footer = () => {
- 
   return (
     <div>
       <p className="bg-zinc-600 dark:bg-zinc-400 w-[100%] h-[0.5px]"></p>
-      <div className="pt-6 pb-12 max-wid flex md:flex-row flex-col text-center justify-between items-center sticky bottom-0">
+      <div className="pt-6 pb-12 max-wid flex md:flex-row flex-col text-center justify-between items-center sticky gap-6 bottom-0">
         <div className="font-semibold">
           Built with: React.js; Tailwind CSS; Vercel
         </div>
-        <h3>
+        {/* <h3>
           Design inspired by{" "}
           <a
             target="_blank"
@@ -17,7 +16,7 @@ const Footer = () => {
             Victor Eke
           </a>
           <span>🙌</span>
-        </h3>
+        </h3> */}
         <div className="font-semibold">
           Copyright &copy; Emmanuel Oladiran 2024 All rights Reserved
         </div>
