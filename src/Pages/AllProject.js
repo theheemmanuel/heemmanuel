70b6bdfa27dbdcf147ab8d7c0b877a42cc7de137
theheemmanuel <oladirane@gmail.com>
2026-03-22 05:58:19 +0100

    added 4t luxury hotel

--- src/Pages/AllProject.js
+++ src/Pages/AllProject.js
@@ -7,11 +7,36 @@ import ENCdetails from "../assets/ENC.png";
 import Jot from "../assets/jot.png";
 import JotDetails from "../assets/Jotmain.png";
 import Art from "../assets/LogoYellow copy.png";
-import ArtDetails from "../assets/Screenshot 2024-10-16 at 11.28.16 AM.png";
+import ArtDetails from "../assets/ArtByFemi.png";
+import tt from "../assets/4T.png";
+import ttlogo from "../assets/4T Luxury Hotel Logo Black.png";
 
 const Allprojects = [
   {
     id: 1,
+    title: "ENCLINKS Tech",
+    subtitle: "Tech Company page",
+    image: ENC,
+    main: ENCdetails,
+    link: "https://enclinks.com",
+    tech: ["React JS", "Tailwind CSS", "Animate on Scroll", "Hostinger"],
+    overview:
+      "ENCLINKS is a tech company landing page which talks about the services offered by the company and as well as their past projects. It was developed using React JS, Tailwind CSS for the styling, added Animate on Scroll to add smooth animations and it was hosted on Hostinger.",
+  },
+
+  {
+    id: 2,
+    title: "4T Luxury Hotel",
+    subtitle: "Hotel Company page",
+    image: ttlogo,
+    main: tt,
+    link: "https://4tluxuryhotel.com",
+    tech: ["Next.js", "Tailwind CSS", "Hostinger"],
+    overview:
+      "4T Luxury Hotel is a luxury hotel landing page located in Osogbo, Osun State. It showcases their rooms (Standard, VIP, and Executive/Suite), services, amenities, and gallery. Guests can also book rooms and make payments directly on the site. Built with Next.js and Tailwind CSS for styling, and hosted on Hostinger.",
+  },
+  {
+    id: 3,
     title: "ArtByFemi",
     subtitle: "Pottery Website",
     image: Art,
@@ -29,18 +54,7 @@ const Allprojects = [
       "ArtByFemi is a pottery website that showcases the work of the artist, Femi. It was developed using React JS, Tailwind CSS for the styling, added Animate on Scroll to add smooth animations, supabase for creating APIs, Email JS for sending email notifications and it was deployed on Hostinger.",
   },
   {
-    id: 2,
-    title: "ENCLINKS Tech",
-    subtitle: "Tech Company page",
-    image: ENC,
-    main: ENCdetails,
-    link: "https://enclinks.com",
-    tech: ["React JS", "Tailwind CSS", "Animate on Scroll", "Hostinger"],
-    overview:
-      "ENCLINKS is a tech company landing page which talks about the services offered by the company and as well as their past projects. It was developed using React JS, Tailwind CSS for the styling, added Animate on Scroll to add smooth animations and it was hosted on Hostinger.",
-  },
-  {
-    id: 3,
+    id: 4,
     title: "JotItDown",
     subtitle: "A Note Taking App",
     image: Jot,
@@ -51,7 +65,7 @@ const Allprojects = [
       "JotItDown is a versatile note-taking application that empowers users to effortlessly create, organize, modify, and remove notes. Built with React JS, this app features a sleek interface styled using Tailwind CSS and enhanced with Chakra UI components. Users can efficiently categorize their notes into custom folders, streamlining organization and accessibility. The application is deployed on Netlify, ensuring reliable and fast access for users across various devices.",
   },
   {
-    id: 4,
+    id: 5,
     title: "Tranfalgar",
     subtitle: "Healthcare page",
     image: pic2,
@@ -62,7 +76,7 @@ const Allprojects = [
       "Trafalgar is a healthcare landing page which gives an overview of the services rendered by the healthcare, latest articles and as well as testimonials from people who have used their services, it was built with HTML, CSS, and JavaScript. It features a responsive design and smooth animations uisng Animate on Scrill.",
   },
   {
-    id: 5,
+    id: 6,
     title: "GetLinked",
     subtitle: "An Hackathon Project",
     image: Get,
